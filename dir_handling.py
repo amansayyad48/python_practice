1fd2998bf3a8f8735c472fb346cb8e986da48bc6
amansayyad48 <amansayyad48@gmail.com>
2025-02-12 18:45:50 +0530

    added count of line

--- dir_handling.py
+++ dir_handling.py
@@ -11,6 +11,11 @@ for file in os.listdir(dir):
         with open(cur_path, "r") as fp:
             file = fp.readlines()
             # print(file)
+            count = 1
             for line in file:
                 if word in line:
                     print(f"{word} is present in file {cur_path}")
+                    print(f"{word} is present in line no. {count}")
+                    print(f"{word} is present in line {line}")
+                else:
+                    count += 1
